f52accd0ab8d78be253400a8714a4bfe6ecea0d1
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-05-05 10:29:31 -0300

    Create Home page component and add it to AppRouter
    Signed-off-by: Victor Hugo M. Pinto <victor2142@gmail.com>

---
+++ src/Components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Home = () => (
+  <div>
+    <h1>Welcome to BookShare!</h1>
+    <p>Please login or register a new user to get started!</p>
+  </div>
+);
+
+export default Home;
