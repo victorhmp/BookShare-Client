5a1838ff7d809563d65faed26a018c63ec7d014b
kaiquekk <kaique.komata@gmail.com>
2018-05-11 00:55:15 -0300

    Update header style.
    Added style to is-active class of the NavLinks and changed some header class names.
    
    Signed-off-by: kaiquekk <kaique.komata@gmail.com>

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,15 +5,15 @@ const Header = () => (
   <header class="header">
     <h1 class="header__title">BookShare</h1>
     <h2 class="header__subtitle">Sharing your books since 2018.</h2>
-    <div class="flex-container">
-      <div class="link">
-        <NavLink to="/" activeClassName="is-active" exact={true} class="nav">Home</NavLink>
+    <div class="navlink-container">
+      <div class="navlink-div">
+        <NavLink exact to="/" activeClassName="is-active">Home</NavLink>
       </div>
-      <div class="link">
-        <NavLink to="/register" activeClassName="is-active" class="nav">Register</NavLink>
+      <div class="navlink-div">
+        <NavLink to="/register" activeClassName="is-active">Register</NavLink>
       </div>
-      <div class="link">
-        <NavLink to="/login" activeClassName="is-active" class="nav">Login</NavLink>
+      <div class="navlink-div">
+        <NavLink to="/login" activeClassName="is-active">Login</NavLink>
       </div>
     </div>
   </header>
