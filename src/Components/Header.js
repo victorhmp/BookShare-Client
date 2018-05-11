c9c767eea5d51aeea9e3956905a12ac95794a11d
kaiquekk <kaique.komata@gmail.com>
2018-05-10 23:35:35 -0300

    Signed-off-by: kaiquekk <kaique.komata@gmail.com>
    Add style to the header of the app page.
    
    Changed the title of the app page to BookShare.
    Changed the header background color to black, centralized the header text, changed header border radius to a more circular one and added a flex box for the navlinks on the header.

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,11 +2,20 @@ import React from 'react';
 import {NavLink} from 'react-router-dom';
 
 const Header = () => (
-  <header>
-    <h1>BookShare</h1>
-    <NavLink to="/" activeClassName="is-active" exact={true}>Home</NavLink>
-    <NavLink to="/register" activeClassName="is-active">Register</NavLink>
-    <NavLink to="/login" activeClassName="is-active">Login</NavLink>
+  <header class="header">
+    <h1 class="header__title">BookShare</h1>
+    <h2 class="header__subtitle">Sharing your books since 2018.</h2>
+    <div class="flex-container">
+      <div class="link">
+        <NavLink to="/" activeClassName="is-active" exact={true} class="nav">Home</NavLink>
+      </div>
+      <div class="link">
+        <NavLink to="/register" activeClassName="is-active" class="nav">Register</NavLink>
+      </div>
+      <div class="link">
+        <NavLink to="/login" activeClassName="is-active" class="nav">Login</NavLink>
+      </div>
+    </div>
   </header>
 );
 
