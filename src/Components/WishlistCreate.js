caea46a66e1a5e3e675d220e44e73a0de29faead
yurickyh <yurickyussuke@hotmail.com>
2018-05-29 20:50:13 -0300

    Creating the form to create a wishlist in render method.
    Signed-off-by: yurickyh <yurickyussuke@hotmail.com>

--- src/Components/WishlistCreate.js
+++ src/Components/WishlistCreate.js
@@ -12,10 +12,42 @@ class WishlistCreate extends React.Component {
 
     handleChange(e){
         const name = e.target.name;
-        const description = e.target.name;
+        const val = e.target.value;
         this.setState({
             [name]: val,
         });
     }
-        
+
+    render(){
+        return(
+            <section id="header">
+                <div className="section-wrapper-form">
+                    <span class="createWishlist-title">Create Wishlist</span>
+                    <form class="createWishlist-form" onSubmit={(e) => this.props.handleRegisterSubmit(e, this.state)}>
+                        <input
+                            class="input"
+                            type="text"
+                            name="nameWishlist"
+                            placeholder="Name"
+                            value={this.state.name}
+                            onChange={this.handleChange}
+                        />
+                        <input
+                            class="input"
+                            type="textarea"
+                            name="description"
+                            placeholder="Description"
+                            value={this.state.description}
+                            onChange={this.handleChange}
+                        />
+                        <div class="button-container">
+                            <button class="form-button">Create!</button>
+                        </div>
+                    </form>
+                </div>
+            </section>
+        );
+    }        
 }
+
+export default WishlistCreate;
