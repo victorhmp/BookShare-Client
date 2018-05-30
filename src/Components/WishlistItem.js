57988763e9c9f049e739f0b3779f98fff17600aa
yurickyh <yurickyussuke@hotmail.com>
2018-05-30 00:17:10 -0300

    Creating all the methods for WishlistItem Component: constructor method, componentDidMount method and render method.
    Signed-off-by: yurickyh <yurickyussuke@hotmail.com>

---
+++ src/Components/WishlistItem.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import axios from 'axios';
+
+import Auth from '../Modules/Auth';
+
+class WishlistItem extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            myItem: null,
+            itemListLoaded: null,
+        }
+    }
+
+    componentDidMount(){
+        axios.get('http://localhost:3000/wishlist_items', {
+            headers:{
+                token: Auth.getToken(),
+                'Authorization' : `Token ${Auth.getToken()}`,
+            }
+        }).then((response) => {
+            console.log(response.data);
+            this.setState({
+                myItem: response.data.item,
+                itemListLoaded: true
+            })
+        }).catch((err) => console.log(err));
+    }
+
+    render(){
+        return(
+            <section id="itemList">
+                <div className="section-wrapper">
+                    {(this.state.itemListLoaded)
+                    ? this.state.myItem.map(itm => {
+                        return(
+                            <div> key={itm.id}>
+                                <h1>Livro: {itm.book_title}</h1>
+                                <p>Autor: {book_author}</p>
+                                <p>Editora: {book_publication}</p>
+                            </div>
+                        );
+                    })
+                    : <p>Loading...</p>
+                    }
+                </div>
+            </section>
+        )
+    }
+}
+
+export default WishlistItem;
