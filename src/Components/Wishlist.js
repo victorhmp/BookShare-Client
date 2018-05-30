774baebedcca6cf2a180f473f3953e312b8af951
yurickyh <yurickyussuke@hotmail.com>
2018-05-29 22:45:05 -0300

    Creating a button to wishlistCreate navlink.
    Signed-off-by: yurickyh <yurickyussuke@hotmail.com>

--- src/Components/Wishlist.js
+++ src/Components/Wishlist.js
@@ -31,7 +31,6 @@ class Wishlist extends React.Component {
     return (
       <section id="wishlists">
         <div className="section-wrapper">
-        <NavLink to={"/wishlistsCreate"} exact={true}>New Wishlist</NavLink>
           {(this.state.wishlistsLoaded) 
           ? this.state.myWishlists.map(wishlist => {
             return (
@@ -42,7 +41,12 @@ class Wishlist extends React.Component {
             );
           })
           : <p>Loading...</p>
-          }          
+          }
+          <div className="button-container">
+            <button className="form-button">
+              <NavLink to={"/wishlistsCreate"} exact={true}>New Wishlist</NavLink>
+            </button>            
+          </div>          
         </div>
       </section>
     );
