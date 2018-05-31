74e431823fc4e90f5069c1fe7d844d75f7e00c0b
yurickyh <yurickyussuke@hotmail.com>
2018-05-30 23:11:35 -0300

    Creating the form to create a item in render function.
    Signed-off-by: yurickyh <yurickyussuke@hotmail.com>

--- src/Components/WishlistItemCreate.js
+++ src/Components/WishlistItemCreate.js
@@ -1,24 +1,62 @@
 import React from 'react';
-import axios from 'axios';
 
-import Auth from '../Modules/Auth';
-
-class Wishlist extends React.Component {
+class WishlistItemCreate extends React.Component {
     constructor(){
         super();
         this.state = {
-            name: "",
-            description: "",
+            book_title: '',
+            book_author: '',
+            book_publication: '',
         }
         this.handleChange = this.handleChange.bind(this);
     }
 
     handleChange(e){
         const name = e.target.name;
-        const description = e.target.name;
+        const val = e.target.value;
         this.setState({
             [name]: val,
         });
     }
-        
+
+    render(){
+        return(
+            <section id="header">
+                <div className="section-wrapper-form">
+                    <span className="wishlistItem-title">Create Wishlist Item</span>
+                    <form className="wishlistItem-form" onSubmit={(e) => this.props.handleWishlistItemSubmit(e, this.state)}>
+                        <input
+                            className="input"
+                            type="text"
+                            name="book_title"
+                            placeholder="Title"
+                            value={this.state.book_title}
+                            onChange={this.handleChange}
+                        />
+                        <input
+                            className="input"
+                            type="text"
+                            name="book_author"
+                            placeholder="Author"
+                            value={this.state.book_author}
+                            onChange={this.handleChange}
+                        />
+                        <input
+                            className="input"
+                            type="text"
+                            name="book_publication"
+                            placeholder="Publishing Company"
+                            value={this.state.book_publication}
+                            onChange={this.handleChange}
+                        />
+                        <div className="button-container">
+                            <button className="form-button">Create WishItem!</button>                            
+                        </div>
+                    </form>
+                </div>
+            </section>
+        );
+    }        
 }
+
+export default WishlistItemCreate;
