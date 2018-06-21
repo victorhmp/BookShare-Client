9d1c6af96903d21ebf5327c92fd7104402f017f9
yurickyh <yurickyussuke@hotmail.com>
2018-06-21 20:48:42 -0300

    Fixing indentation.
    Signed-off-by: yurickyh <yurickyussuke@hotmail.com>

--- src/Components/WishlistCreate.js
+++ src/Components/WishlistCreate.js
@@ -2,51 +2,51 @@ import React from 'react';
 
 class WishlistCreate extends React.Component {
     constructor(){
-        super();
-        this.state = {
-            name: '',
-            description: '',
-        }
-        this.handleChange = this.handleChange.bind(this);
+      super();
+      this.state = {
+        name: '',
+        description: '',
+      }
+      this.handleChange = this.handleChange.bind(this);
     }
 
     handleChange(e){
-        const name = e.target.name;
-        const val = e.target.value;
-        this.setState({
-            [name]: val,
-        });
+      const name = e.target.name;
+      const val = e.target.value;
+      this.setState({
+        [name]: val,
+      });
     }
 
     render(){
-        return(
-            <section id="header">
-                <div className="section-wrapper-form">
-                    <span className="wishlist-title">Create Wishlist</span>
-                    <form className="wishlist-form" onSubmit={(e) => this.props.handleWishlistSubmit(e, this.state)}>
-                        <input
-                            className="input"
-                            type="text"
-                            name="name"
-                            placeholder="Name"
-                            value={this.state.name}
-                            onChange={this.handleChange}
-                        />
-                        <input
-                            className="input"
-                            type="text"
-                            name="description"
-                            placeholder="Description"
-                            value={this.state.description}
-                            onChange={this.handleChange}
-                        />
-                        <div className="button-container">
-                            <button className="form-button">Create!</button>                            
-                        </div>
-                    </form>
-                </div>
-            </section>
-        );
+      return(
+        <section id="header">
+          <div className="section-wrapper-form">
+            <span className="wishlist-title">Create Wishlist</span>
+            <form className="wishlist-form" onSubmit={(e) => this.props.handleWishlistSubmit(e, this.state)}>
+              <input
+                className="input"
+                type="text"
+                name="name"
+                placeholder="Name"
+                value={this.state.name}
+                onChange={this.handleChange}
+              />
+              <input
+                className="input"
+                type="text"
+                name="description"
+                placeholder="Description"
+                value={this.state.description}
+                onChange={this.handleChange}
+              />
+              <div className="button-container">
+                <button className="form-button">Create!</button>                            
+              </div>
+            </form>
+          </div>
+        </section>
+      );
     }        
 }
 
