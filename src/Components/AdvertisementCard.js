39f5f59339dc6ee3eb77ea3d8db19fc789b13b23
Ricardo Kojo <ricardokojo7@gmail.com>
2018-06-26 17:07:43 -0300

    Update AdvertisementCard style at My Advertisements page
    Add margin to large button css class (btn-lg);
    Add "Editar" button to Card;
    Change Card css class names to more generic names
    
    Signed-off-by: Ricardo Kojo <ricardokojo7@gmail.com>

--- src/Components/AdvertisementCard.js
+++ src/Components/AdvertisementCard.js
@@ -2,22 +2,18 @@ import React from 'react';
 import placeholder from '../images/placeholder.png'
 
 class AdvertisementCard extends React.Component {
-  constructor() {
-    super();
-  }
-
   render() {
     return(
       (this.props.type === "feed") ? 
         <div id={this.props.adv.id} className='card-adv'>
           <img src={placeholder} alt='Imagem do livro anunciado' className="card-adv__book-image" />
-          <div className='card-adv__book-info col'>
+          <div className='card-adv__card-info col'>
             <h2 className='title'>{this.props.adv.book_title}</h2>
             <p className='author'> <b>Autor:</b> {this.props.adv.book_author}</p>
             <p className='publication'> <b>Editora:</b> {this.props.adv.book_publication}</p>
             <p className='comment'> <b>Comentário do Usuário:</b> {this.props.adv.comment}</p>
           </div>
-          <div className='card-adv__user-info'>
+          <div className='card-adv__right-info'>
             <p className='created-at'> <b>Criado às:</b> {this.props.adv.created_at} </p>
             <p className='username'> <b>Por:</b> {this.props.adv.user.username}</p>
             <button className="btn btn-blue">Ofertar</button>
@@ -25,16 +21,17 @@ class AdvertisementCard extends React.Component {
         </div> 
       :
       <div id={this.props.adv.id} className='card-adv'>
-        <div className='card-adv__book-info row'>
+        <div className='card-adv__card-info row'>
           <h2 className='title'>{this.props.adv.book_title}</h2>
           <p className='author'> <b>Autor:</b> {this.props.adv.book_author}</p>
           <p className='publication'> <b>Editora:</b> {this.props.adv.book_publication}</p>
           <p className='comment'> <b>Comentário:</b> {this.props.adv.comment}</p>
-        </div>
-        <div className='card-adv__user-info'>
           <p className='created-at'> <b>Criado às:</b> {this.props.adv.created_at} </p>
           <p className='status'> <b>Status:</b> {this.props.adv.status} </p>
-          <button className="btn btn-blue">Editar</button>
+          <div className='card-adv__right-info'>
+            <button className="btn-lg btn-blue">Editar</button>
+            <button className="btn btn-blue">Fechar anúncio</button>
+          </div>
         </div>
       </div> 
     )
