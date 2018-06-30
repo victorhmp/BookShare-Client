78296407593d7d3d4fa38ea4a21da22591619bc2
ricardokojo <ricardokojo7@gmail.com>
2018-06-30 04:53:40 -0300

    Update My Trades page to show all trades
    Now shows trades that came from user's Advertisements
    
    Signed-off-by: ricardokojo <ricardokojo7@gmail.com>

--- src/Components/Trade.js
+++ src/Components/Trade.js
@@ -35,8 +35,19 @@ class Trade extends React.Component {
     return (
       <section id="my-offers">
         <div className="section-wrapper">
+        <h2>Trocas vindas de ofertas feitas</h2>
           {(this.state.dataLoaded) 
           ? this.state.myOfferTrades.map(t => {
+            return (
+              <TradeCard key={t.id} trade={t}/>
+            );
+          })
+          : <div className="loader" />
+          }
+
+          <h2>Trocas vindas de um anúncio próprio</h2>
+          {(this.state.dataLoaded) 
+          ? this.state.myAdvertisementTrades.map(t => {
             return (
               <TradeCard key={t.id} trade={t}/>
             );
