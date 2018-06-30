320a0e4cbe3eca5ad128cffcd7c354d79639c5bd
Victor Hugo M. Pinto <victor2142@gmail.com>
2018-06-29 21:19:19 -0300

    Make profile page display correct info
    Use setState to update component state with user info using response
    from the API.
    
    Add Gravatar component to display profile picture based on user's email.
    
    Signed-off-by: Victor Hugo M. Pinto <victor2142@gmail.com>

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import Gravatar from 'react-gravatar';
 
 import Auth from '../Modules/Auth';
 
@@ -9,6 +10,7 @@ class Profile extends React.Component {
     this.state = {
       username: null,
       email: null,
+      name: null,
       location: null,
     }
   }
@@ -21,12 +23,29 @@ class Profile extends React.Component {
       }
     }).then((response) => {
       console.log(response);
+      this.setState({
+        username: response.data.user.username,
+        email: response.data.user.email,
+        name: response.data.user.name
+      })
     }).catch((err) => console.log(err));
   }
 
   render() {
     return(
-      <h1>Página de perfil</h1>
+      <section className="profile-container">
+        <div className="profile__card">
+          <div className="profile__title">
+            <h1>{this.state.username ? this.state.username : "User Profile"}</h1>
+          </div>
+          <div className="profile__gravatar">
+            <Gravatar email={this.state.email} size={300} className="profile__gravatar"/>
+          </div>
+          <div className="profile__info">
+            <p><span className="email">Email: </span>{this.state.email}</p>
+          </div>
+        </div>
+      </section>
     );
   }
 }
